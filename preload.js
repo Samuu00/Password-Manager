0056d73774785b9e47c974a5de4950a8dcd97236
Samuu00 <148590518+Samuu00@users.noreply.github.com>
2025-11-20 14:35:37 +0100

    Delete preload.js

--- preload.js
+++
@@ -1,7 +0,0 @@
-const { contextBridge, ipcRenderer } = require('electron');
-
-contextBridge.exposeInMainWorld('electronAPI', {
-    getPasswords: () => ipcRenderer.invoke('get-passwords'),
-    addPassword: (entry) => ipcRenderer.invoke('add-password', entry),
-    deletePassword: (id) => ipcRenderer.invoke('delete-password', id)
-});
